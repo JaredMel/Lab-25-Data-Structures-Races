9d78c9b076c47e6f31ee1c455ad9476b702f7e04
JaredMel <jareddmelendez1764@gmail.com>
2024-10-24 01:13:17 -0700

    WIP

--- main.cpp
+++ main.cpp
@@ -22,65 +22,105 @@ int main() {
     string code;
 
     auto start = high_resolution_clock::now();
-    while (fin >> vector1[i++]);
+    while (fin)
+    {
+        fin >> code;
+        vector1[i] = code;
+        i++;
+    }
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start);
     cout << duration.count() << "   ";
 
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     while (fin)
     {
         fin >> code;
         list1.push_back(code);
     }
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(end - start);
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
     cout << duration.count() << "   ";
 
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     while (fin)
     {
         fin >> code;
         set1.insert(code);
     }
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(end - start);
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
     cout << duration.count() << endl;
 
     fin.close();
     //Sorting
     cout << "Sort   ";
 
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     sort(vector1.begin(), vector1.end());
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(end - start);
-    cout << duration.count() << endl;
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << duration.count() << "   ";
 
-    auto start = high_resolution_clock::now();
+    start = high_resolution_clock::now();
     list1.sort();
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(end - start);
-    cout << duration.count() << endl;
-    
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << duration.count() << "   ";
+
+    cout << "-1" << endl;
+
     //Inserting
+    cout << "Insert   ";
     string test = "TESTCODE";
     int mid = 10000;
+
+    start = high_resolution_clock::now();
     vector1.insert(vector1.begin() + mid, test);
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << duration.count() << "   ";
+
+    start = high_resolution_clock::now();
     list<string> :: iterator it1 = list1.begin();
     advance(it1, mid);
     list1.insert(it1, test);
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << duration.count() << "   ";
+
+    start = high_resolution_clock::now();
     set<string> :: iterator it2 = set1.begin();
     advance(it2, mid);
     set1.insert(it2, test);
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << duration.count() << endl;
+
     //Deleting
+    cout << "Delete   ";
+
+    start = high_resolution_clock::now();
     vector1.erase(vector1.begin()+19999);
-    list<string> :: iterator it1 = list1.begin();
-    advance(it1, mid);
-    list1.erase(it1);
-    set<string> :: iterator it2 = set1.begin();
-    advance(it2, mid);
-    set1.erase(it2);
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << duration.count() << "   ";
+
+    start = high_resolution_clock::now();
+    list<string> :: iterator it3 = list1.begin();
+    advance(it3, mid);
+    list1.erase(it3);
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << duration.count() << "   ";
+
+    start = high_resolution_clock::now();
+    set<string> :: iterator it4 = set1.begin();
+    advance(it4, mid);
+    set1.erase(it4);
+    end = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(end - start);
+    cout << duration.count() << endl;
     
     
     
